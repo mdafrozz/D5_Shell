b8a6b57bfaa45a974908239bd1020fde75eedaa3
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 16:52:37 +0530

    Update and rename prob02.sh to Day&Month.sh
    Print True and False for Day and Month

--- Selection/Day&Month.sh
+++ Selection/Day&Month.sh
@@ -1,5 +1,4 @@
-echo "program to take day and month from command line and print true if day of month is between March 20 and June 20, false otherwise
-"
+echo "program to take day and month from command line and print true if day of month is between March 20 and June 20, false otherwise"
 date1="0319"
 date2="0621"
 echo "please enter the date in mmdd format:"
